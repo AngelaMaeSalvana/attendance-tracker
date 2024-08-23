f009309cf945cc541d614a2ff69a810b7e8d0003
Angela Mae Salvaña <120080231+AngelaMaeSalvana@users.noreply.github.com>
2024-08-23 15:56:51 +0800

    Login Page

---
+++ client/src/Components/Dashboard/Dashboard.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Dashboard = () => {
+  return (
+    <div>
+        <h1>HOME</h1>
+        <a href="/register">Register</a>
+        <a href="/">Login</a>
+    </div>
+  )
+}
+
+export default Dashboard
---
+++ client/src/Components/Register/Register.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Register.css'
+
+const Register = () => {
+  return (
+    <div>
+        <h1>REGISTER</h1>
+        <a href="/">Login</a>
+        <a href="/dashboard">Home</a>
+    </div>
+  )
+}
+
+export default Register
